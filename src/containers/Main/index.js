e11801469586c4ac99fed772c16541d746d5a2a4
ISLOM QURBONOV <islom_uz@inbox.ru>
2018-07-06 16:19:21 +0500

    must do mobile version of TWIG browser detector is ready

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -7,6 +7,8 @@ import { fetchPosts, getPostId, likePost, unlikePost } from "../../actions/postA
 import { fetchUser } from "../../actions/auth";
 import { bindActionCreators } from 'redux'
 
+import { BrowserView, MobileView } from 'react-device-detect'
+
 import Icon from 'react-icons-kit'
 import {comment} from 'react-icons-kit/fa/comment'
 import {share} from 'react-icons-kit/fa/share'
@@ -54,56 +56,61 @@ class Main extends Component{
 
         return (
             <div id="main">
-                <InfiniteScroll
-                    dataLength={this.state.items.length}
-                    next={this.fetchMoreData}
-                    hasMore={true}
-                >
-                {this.state.items.map((post, index) => (
-                        <div key={index} className="posts-container" id={"post__"+index} onClick={()=>this.props.getPostId(post.id)}>
-                            <div className='post-header'>
-                                <h3><Link to={"post/test_"+post.id} >{post.title}</Link></h3>
-                                <div className='user-data'>
-                                    <span><Moment  fromNow>{post.date}</Moment></span>
-                                    <p>author <a href="">u/{post.user.username}</a></p>
-                                    <p>from <a href="">t/{post.twig.twig_name}</a></p>
-                                </div>
-                            </div>
-
-                            <div className='post-body'>
-                                {post.media_type==='image'
-                                    ? <img width='100%' src={post.media[0]} alt=""/>
-                                    : <video width="100%" height="400" src={post.media[0]} frameBorder="0" controls allowFullScreen></video>
-                                }
-                            </div>
-                            <div className="post-footer">
-                                <div className="footer-left">
-                                    <div className="footer-comment" >
-                                        <Icon icon={comment} />
-                                    </div>
-                                    <div className="footer-share">
-                                        <Icon icon={share} />
+                <BrowserView>
+                    <InfiniteScroll
+                        dataLength={this.state.items.length}
+                        next={this.fetchMoreData}
+                        hasMore={true}
+                    >
+                        {this.state.items.map((post, index) => (
+                            <div key={index} className="posts-container" id={"post__"+index} onClick={()=>this.props.getPostId(post.id)}>
+                                <div className='post-header'>
+                                    <h3><Link to={"post/test_"+post.id} >{post.title}</Link></h3>
+                                    <div className='user-data'>
+                                        <span><Moment  fromNow>{post.date}</Moment></span>
+                                        <p>author <a href="">u/{post.user.username}</a></p>
+                                        <p>from <a href="">t/{post.twig.twig_name}</a></p>
                                     </div>
                                 </div>
 
-                                <div className="footer-right">
-                                    <div className="unlike">
-                                        <p></p>
-                                        <Icon  onClick={()=> this.props.unlikePost(post.id)} icon={arrowDown} />
+                                <div className='post-body'>
+                                    {post.media_type==='image'
+                                        ? <img width='100%' src={post.media[0]} alt=""/>
+                                        : <video width="100%" height="400" src={post.media[0]} frameBorder="0" controls allowFullScreen></video>
+                                    }
+                                </div>
+                                <div className="post-footer">
+                                    <div className="footer-left">
+                                        <div className="footer-comment" >
+                                            <Icon icon={comment} />
+                                        </div>
+                                        <div className="footer-share">
+                                            <Icon icon={share} />
+                                        </div>
                                     </div>
-                                    <div className="count">
 
-                                    </div>
-                                    <div className="like" >
-                                        <Icon  onClick={()=> this.props.likePost(post.id)} icon={arrowUp} />
-                                        <p></p>
+                                    <div className="footer-right">
+                                        <div className="unlike">
+                                            <p></p>
+                                            <Icon  onClick={()=> this.props.unlikePost(post.id)} icon={arrowDown} />
+                                        </div>
+                                        <div className="count">
+
+                                        </div>
+                                        <div className="like" >
+                                            <Icon  onClick={()=> this.props.likePost(post.id)} icon={arrowUp} />
+                                            <p></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                ) )}
-                </InfiniteScroll>
+                        ) )}
+                    </InfiniteScroll>
+                </BrowserView>
+                <MobileView>
+                    <h1>Mobile? bye!</h1>
+                </MobileView>
             </div>
         )
     }
